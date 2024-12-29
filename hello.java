7a3cd08ffef212c0dbfddc9dc2e4020c2642db87
Pinky Patel <pinkypatel@Pinkys-MacBook-Air.local>
2024-12-29 11:40:43 -0600

    hello.java

--- hello.java
+++ hello.java
@@ -1,5 +1,5 @@
 public class hello {
     public static void main(String[] args) {
-        System.out.println("Hello, \nThis is my first program in the Java\nWelcome to the World");
+        System.out.println("Hello, \nThis is my first program in the Java\nWelcome to the World\n hello Pinky");
     }
 }
